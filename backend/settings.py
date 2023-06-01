19af7c75fdbd7cbfbcda43de997354d68feae58d
Tyler <tylermauch18@gmail.com>
2023-06-01 18:08:56 -0400

    whitelisting

--- backend/settings.py
+++ backend/settings.py
@@ -62,15 +62,17 @@ CORS_ORIGIN_WHITELIST = [
     'http://10.0.0.18:3000',  # frontend
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'wsmapi.onrender.com'
+    'wsmapi.onrender.com',
+    '216.24.57.3:443',
 ]
 CORS_ALLOWED_ORIGINS = [
     'http://10.0.0.18:3000',
     'http://localhost:3000',
     'http://localhost:8000',
     'http://127.0.0.1:9000',
-    'http://127.0.0.1:3000'
-    'wsmapi.onrender.com'
+    'http://127.0.0.1:3000',
+    'wsmapi.onrender.com',
+    '216.24.57.3:443',
 ]
 
 ROOT_URLCONF = 'backend.urls'
