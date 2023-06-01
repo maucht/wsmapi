319180e80c11718c172424f653e29f6926273b19
Tyler <tylermauch18@gmail.com>
2023-06-01 17:33:18 -0400

    debug off

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)yhqk%*#&$xu@c!=yqfmu&*%@26@e1zquo81tf^!sk#-tx5=+4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
